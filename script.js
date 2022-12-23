fba9dfd7ba46377ef9a8a96eac399c84d3a6d0a8
dacoder101 <107454678+dacoder101@users.noreply.github.com>
2022-12-22 22:29:08 -0800

    Added Main Files
    Added Main Files

---
+++ script.js
@@ -0,0 +1,11 @@
+function randnum(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function oohlala() {
+  while (true) {
+    alert("hey! never gonna give never gonna give; give u up")
+  }
+}
+function bericked() { 
+  document.getElementById("body").style.backgroundImage = "url(Resources/norickboi.jpeg)"
+}
